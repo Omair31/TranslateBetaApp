4310d1151a27b5a7998f7de54292b99e6bd4249d
Omeir Ahmed <omair.ahmed@vyro.ai>
2024-06-14 00:01:44 +0500

    Initial Commit

---
+++ TranslateBetaApp/TranslateBetaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateBetaAppApp.swift
+//  TranslateBetaApp
+//
+//  Created by M on 14/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TranslateBetaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
